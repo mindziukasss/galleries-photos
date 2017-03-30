1a96080707f4241773ce8bf70aff7e1ae9e27a04
mindziukasss <mindziukasss@gmail.com>
2017-03-30 16:06:54 +0300

    Fixed controllers and added templates

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PhotoControler extends Controller
+class GalleryController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class PhotoControler extends Controller
      */
     public function index()
     {
-        //
+        return view('gallery/index');
     }
 
     /**
@@ -23,7 +23,7 @@ class PhotoControler extends Controller
      */
     public function create()
     {
-        //
+        return view('gallery/create');
     }
 
     /**
